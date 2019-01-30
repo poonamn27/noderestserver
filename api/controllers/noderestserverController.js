888efea4956901474ec491016cbe9f0daeae28cd
root <root@ip-172-31-28-206.us-west-2.compute.internal>
2019-01-30 18:21:18 +0000

    test

---
+++ api/controllers/noderestserverController.js
@@ -0,0 +1,4 @@
+exports.list_all_tasks = function(req, res) {
+  console.log("test")
+  res.json("test")
+};
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 4500;
+  port = process.env.PORT || 8080;
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
 
-app.listen(port);
+
+var routes = require('./api/routes/noderestserverRoutes'); //importing route
+routes(app);
+  app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
